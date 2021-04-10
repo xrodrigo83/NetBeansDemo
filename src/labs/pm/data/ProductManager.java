12cd1f84938b4ccd3ae5584b11cd647680d871c0
Rodrigo Rodrigues <Rodrigo Rodrigues@NT-Rodrigo>
2021-04-10 19:50:17 -0300

    Aula 10 finalizada.

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -24,6 +24,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -125,6 +126,20 @@ public class ProductManager {
             txt.append(formatter.getText("no.reviews"));
             txt.append('\n');
         }
+        System.out.println(txt);
+    }
+
+    public void printProducts(Comparator<Product> sorter) {
+        List<Product> productList = new ArrayList<>(products.keySet());
+        productList.sort(sorter);
+
+        StringBuilder txt = new StringBuilder();
+
+        for (Product product : productList) {
+            txt.append(formatter.formatProduct(product));
+            txt.append('\n');
+        }
+
         System.out.println(txt);
     }
 
